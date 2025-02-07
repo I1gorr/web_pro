034448972436e36b1aa20ef2bb000cd507713783
I1gorr <kushagrasingh1424@gmail.com>
2025-02-07 23:42:43 +0530

    Updated UI

---
+++ web_pro/AboutSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutSection() {
+  return (
+    <section className="about-section">
+      <h2>About EduShare</h2>
+      <p>
+        EduShare is an innovative platform designed to help students collaborate, share
+        knowledge, and prepare for exams together.
+      </p>
+      <p>
+        Our goal is to make learning more accessible, engaging, and fun by providing a
+        variety of student-contributed resources.
+      </p>
+    </section>
+  );
+}
+
+export default AboutSection;
---
+++ web_pro/src/AboutSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function AboutSection(){
+
+return(
+        <div className='AboutSection'>
+        <h1>Welcome to EduShare!</h1>
+        <div className='keypoints'>
+          <p>Brighten up your day by diving into a sea of knowledge, contributed by students just like you.</p>
+          <p>Say goodbye to the days when exam preparation was a solitary chore. Join us on this extraordinary journey for an immersive learning experience.</p>
+          <p>The one-stop solution for all your educational needs, crafted for students, by students. We bring you a rich and varied collection of notes ready for you to devour.</p>
+        </div>
+      </div>
+);
+}
+      export default AboutSection;
